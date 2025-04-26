eb54b1b7d36b8f803dd76302be62bf0ddd290d23
sathya9100 <ankitha256718@gmail.com>
2025-04-25 20:23:53 -1200

    Add files via upload

---
+++ coverting a list.py
@@ -0,0 +1,10 @@
+#convert a list to array
+import array
+my_list=[1,2,3,4,5]
+array_from_list=array.array('i',my_list)#'i' denotes integer type
+#convert a tuple to an array
+my_tuple=(6,7,8,9,10)
+array_from_tuple=array.array('i', my_tuple)#'i' denotes integer type
+#print the arrays
+print("Array from list:",array_from_list)
+print("Array from tuple:",array_from_tuple)
