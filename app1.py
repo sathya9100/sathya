401ca9153321f250638489d27b2d10ff952603dd
sathya9100 <ankitha256718@gmail.com>
2025-05-02 21:25:00 -1200

    Add files via upload

--- app1.py
+++ app1.py
@@ -1,10 +1,8 @@
-row=int(input("Enter No of Rows for 1st Matrix:"))
-column=int(input("Enter No of column for 1nd Matrix:"))
-row1=int(input("Enter No of Rows for 2st Matrix:"))
-column1=int(input("Enter No of column for 2nd Matrix:"))
-X = [[int(input(("Enter value for X[",i,"][",j,"]:")))
-for j in range(column)] for i in range(row)]
-Y = [[int(input(("Enter value for Y[",i,"][",j,"]:")))
-for j in range(column1)] for i in range(row1)]
-print("1st Matrix X:",X)
-print("2st Matrix Y:",Y)
+with open("file1.txt", "r") as fh1:
+    with open("file2.txt", "r") as fh2:
+        with open("mergefile.txt", "w") as fh3:
+            # Read all lines from both files and combine them into a list 'q'
+            q = fh1.readlines() + fh2.readlines()
+            
+            # Write the combined lines into the output file 'mergefile.txt'
+            fh3.writelines(q)
